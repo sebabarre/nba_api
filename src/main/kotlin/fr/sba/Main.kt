1a359ac125fa381d03792d9811f86dffcecdfc5d
Sébastien Barre <sbarre.prestataire@oui.sncf>
2019-10-09 10:53:28 +0200

    update

--- src/main/kotlin/fr/sba/Main.kt
+++ src/main/kotlin/fr/sba/Main.kt
@@ -2,10 +2,18 @@ package fr.sba
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import io.javalin.Javalin
+import org.apache.logging.log4j.LogManager
+
+private val logger = LogManager.getLogger("Main")
 
 fun main() {
+
+
     val api = Javalin.create { config ->
         config.enableCorsForAllOrigins()
+        config.requestLogger { ctx, timeMs ->
+            logger.debug("${ctx.attribute<String>("id")} : ${ctx.method()} ${ctx.path()} took $timeMs ms and respond ${ctx.status()}")
+        }
     }.start(7777)
     val mapper = jacksonObjectMapper()
     val rapidApi = RapidApi()
@@ -13,8 +21,12 @@ fun main() {
         it.result("ok")
     }
     api.get("/standings") {
+        logger.debug("calling /standings")
         it.result(mapper.writeValueAsString(rapidApi.getNbaRanking()))
     }
-    api.get("/pronos") { it.result(rapidApi.getRuinartRanking())}
+    api.get("/pronos") {
+        logger.debug("calling /pronos")
+        it.result(rapidApi.getRuinartRanking())
+    }
 }
 
--- src/main/kotlin/fr/sba/RapidApi.kt
+++ src/main/kotlin/fr/sba/RapidApi.kt
@@ -7,6 +7,9 @@ import com.squareup.okhttp.Request
 import fr.sba.data.*
 import org.apache.logging.log4j.LogManager
 import org.slf4j.LoggerFactory
+import java.time.ZonedDateTime
+import java.util.*
+import kotlin.concurrent.fixedRateTimer
 
 const val HEADER_API_KEY = "x-rapidapi-key"
 const val HEADER_API_HOST = "x-rapidapi-host"
@@ -16,12 +19,34 @@ class RapidApi {
     private val mapper = jacksonObjectMapper()
     private val key = "8971ac3280msh73a623b488f4a49p153b68jsn250bd96f8428"
     private val host = "api-nba-v1.p.rapidapi.com"
-    private lateinit var  nbaTeams: List<Pair<String,String>>
+    @Volatile private lateinit var  nbaTeams: List<Pair<String,String>>
+    @Volatile private lateinit var nbaRanking: RuinardStandingTeam
+    @Volatile private var nbOfApiCalls = 0
     private val logger = LogManager.getLogger(this.javaClass)
+    private val EVERY_DAY: Long = 24 * 60 * 60 * 1000L
+
+    init {
+        fixedRateTimer(
+            name = "reset-nb-of-apicall-counter",
+            startAt = Date(ZonedDateTime.now().plusDays(1).withHour(0).withMinute(0).withSecond(0).toInstant().epochSecond),
+            period = EVERY_DAY
+        ) {
+            nbOfApiCalls = 0
+        }
+    }
+
+    @Synchronized
+    private fun incrementCounter() {
+        nbOfApiCalls++
+        logger.debug("Number of api calls today : $nbOfApiCalls")
+    }
 
     private fun getNbaTeams(): List<Pair<String,String>> {
-        if (::nbaTeams.isInitialized) return nbaTeams
-        logger.debug("calling api to get nba teams")
+        if (::nbaTeams.isInitialized) {
+            logger.debug("No need to call api, nbaTeams is already initialized")
+            return nbaTeams
+        }
+        logger.debug("Calling api to get nba teams")
         val client = OkHttpClient()
 
         val request = Request.Builder()
@@ -30,10 +55,11 @@ class RapidApi {
             .addHeader(HEADER_API_HOST, host)
             .addHeader(HEADER_API_KEY, key)
             .build()
-
+        incrementCounter()
         val response = client.newCall(request).execute()
         val mapResponse =  mapper.readValue<TeamResponse>(response.body().string())
-        return mapResponse.api.teams.filter { it.nbaFranchise == "1" }.map { it.teamId to it.city }
+        nbaTeams = mapResponse.api.teams.filter { it.nbaFranchise == "1" }.map { it.teamId to it.city }
+        return nbaTeams
     }
 
     private fun getStandings(conf: Conference): String {
@@ -46,7 +72,7 @@ class RapidApi {
             .addHeader(HEADER_API_HOST, host)
             .addHeader(HEADER_API_KEY, key)
             .build()
-
+        incrementCounter()
         val response = client.newCall(request).execute()
         return response.body().string()
     }
@@ -55,18 +81,28 @@ class RapidApi {
         WEST, EAST
     }
 
+    @Synchronized
     fun getNbaRanking(): RuinardStandingTeam {
-        return RuinardStandingTeam(east = getRankingByConf(Conference.EAST), west = getRankingByConf(Conference.WEST))
+        if (::nbaRanking.isInitialized) {
+            logger.debug("No need to call api, nbaRanking is already initialized")
+            return nbaRanking
+        }
+        logger.debug("Calling getNbaRanking")
+
+        nbaRanking = RuinardStandingTeam(east = getRankingByConf(Conference.EAST), west = getRankingByConf(Conference.WEST))
+        return nbaRanking
     }
 
     private fun getRankingByConf(conference: Conference): List<RuinartStandingByConference> {
-        return listOf(RuinartStandingByConference(teamName = "BOSTON FUCKERS", ranking = 1, win = 81, losses = 1))
-        /*return mapper.readValue<StandingResponse>(getStandings(conference)).api.standings.map {
+        logger.debug("calling getRankingByConf $conference")
+
+//        return listOf(RuinartStandingByConference(teamName = "BOSTON FUCKERS", ranking = 1, win = 81, losses = 1))
+        return mapper.readValue<StandingResponse>(getStandings(conference)).api.standings.map {
             val teamName = getNbaTeams().find { team ->
                 team.first == it.teamId
             }?.second
             RuinartStandingByConference(teamName ?: "oups", Integer.valueOf(it.conference.rank), Integer.valueOf(it.win), Integer.valueOf(it.loss))
-        }.sortedBy { it.ranking }*/
+        }.sortedBy { it.ranking }
     }
 
     private fun getPronos(): List<Prono> = mapper.readValue(fileToString(javaClass.classLoader.getResourceAsStream("pronos.json")))
